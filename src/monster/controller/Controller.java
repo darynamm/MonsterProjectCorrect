d25e7d4d197ac96a4c25413b9cbd97430765c551
darynamm <darynamanuilenko@gmail.com>
2022-08-31 09:48:33 -0600

    we created another monster

--- src/monster/controller/Controller.java
+++ src/monster/controller/Controller.java
@@ -3,14 +3,17 @@ import monster.model.MarshmallowMonster;
 public class Controller
 {
 	private MarshmallowMonster basicMonster; 
+	private MarshmallowMonster myMonster;
 	
 	public Controller()
 	{
 		this.basicMonster = new MarshmallowMonster();
+		this.myMonster = new MarshmallowMonster("Carrot", 2000, 100, true, true, 0.9, 99.999);
 	}
 	public void start()
 	{
 		System.out.println("i made a monster!");
 		System.out.println(basicMonster);
+		System.out.println(myMonster);
 	}
 }
